5eb91f7aeac1e802207dce4efa330d42ea9656c6
Allan Nono <54485659+anonomf@users.noreply.github.com>
2020-11-05 11:30:20 -0300

    TestandoTurma

---
+++ src/app/turma/shared/turma.model.ts
@@ -0,0 +1,7 @@
+export interface Turma{
+    cdTurma: number,
+    cdCurso: string,
+    turma: string,
+    periodo: string,
+    status: string,
+}
---
+++ src/app/turma/shared/turma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Turma } from './turma.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TurmaService {
+
+  private readonly API = 'http://localhost:8080/turma';
+
+  constructor(private http: HttpClient) { }
+
+  getBuscarTurma(turma: number): Observable<Turma>{
+      return this.http.get<Turma>(`${this.API}/${turma}`);  //http://localhost:8080/turma/1
+  }
+
+  postInserirTurma(turma: Turma): Observable<any>{
+    return this.http.post<any>(`${this.API}`, turma);
+  }
+}
