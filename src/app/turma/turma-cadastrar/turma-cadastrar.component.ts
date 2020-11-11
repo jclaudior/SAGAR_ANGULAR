f0262421b009ac6711ab80e25b08c39933937052
Allan Nono <54485659+anonomf@users.noreply.github.com>
2020-11-10 21:58:11 -0300

    DeuRuim1.0

--- src/app/turma/turma-cadastrar/turma-cadastrar.component.ts
+++ src/app/turma/turma-cadastrar/turma-cadastrar.component.ts
@@ -1,8 +1,9 @@
 import { ResponseTurma } from './../shared/responseTurma.model';
 import { TurmaService } from './../shared/turma.service';
 import { Turma } from '../shared/turma.model';
-import { FormsModule }   from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm }   from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-turma-cadastrar',
@@ -10,6 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./turma-cadastrar.component.css']
 })
 export class TurmaCadastrarComponent implements OnInit {
+
+  @ViewChild('formDisciplina', {static: true}) formDisciplina: NgForm;
+
+  requestSucess = false;
+  mensagemModal = '';
+  titleModal = '';
+  cadastrando = false;
+
+  btnCadastrar = false;
 
   turma: Turma = {
     cdCodigo: null,
@@ -27,12 +37,48 @@ export class TurmaCadastrarComponent implements OnInit {
   }
 
   cadastrarTurma(): void{
+    console.log(this.turma);
+    this.cadastrando = true;
     this.service.postInserirTurma(this.turma).subscribe(
       response => {
+        console.log(response);
+        this.requestSucess = true;
         this.responseTurma = response;
         console.log(this.responseTurma);
+        this.titleModal = this.responseTurma.mensagem;
+        if (this.responseTurma.retorno != null){
+          this.mensagemModal = `IdTurma: ${this.responseTurma.retorno.cdCodigo} NomeTurma: ${this.responseTurma.retorno.nmTurma}`;
+          this.turma = {
+            cdCodigo: null,
+            nmTurma: null,
+            curso: null,
+            dsPeriodo: null,
+            stTurma: true
+          };
+        }else{
+          this.mensagemModal = this.responseTurma.mensagem;
+        }
+        $('#mensagemModal').modal('show');
+        this.cadastrando = false;
+      },
+      error => {
+        console.log(error);
+        this.requestSucess = false;
+        if (error.error.mensagem != null){
+          this.titleModal = error.error.mensagem;
+          if(error.error.retorno != null){
+            this.mensagemModal = `NomeTurma: ${error.error.retorno.nmTurma}, Já existe: ${error.error.retorno.cdCodigo} `;
+          }else{
+            this.mensagemModal = error.error.mensagem;
+          }
+        }else{
+          this.titleModal = error.name;
+          this.mensagemModal = error.message;
+        }
+        $('#mensagemModal').modal('show');
+        this.cadastrando = false;
       }
-
     );
   }
+
 }
